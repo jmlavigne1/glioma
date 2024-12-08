bf257b696c905c98e5f52fc9a9b510b69f7db993
Joe LaVigne <jmlavigne510@gmail.com>
2024-12-07 21:50:56 -0500

    feature engineering/preprocessing

--- sigmoid function graph.py
+++ sigmoid function graph.py
@@ -11,3 +11,11 @@ glioma_grading_clinical_and_mutation_features = fetch_ucirepo(id=759)
 # data (as pandas dataframes) 
 X = glioma_grading_clinical_and_mutation_features.data.features 
 y = glioma_grading_clinical_and_mutation_features.data.targets 
+
+X.shape, y.shape
+
+# remove Nan values from dataset, and sift through whether using age, gender or other marked values for our sigmoid function. 
+
+
+plt.scatter(x=X, y=y, color='black')
+plt.show()
