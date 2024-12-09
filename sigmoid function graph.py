bc75423121ddd857842a994f14c84a52594bbe41
Joe LaVigne <jmlavigne510@gmail.com>
2024-12-09 11:29:09 -0500

    sigmoidal classification without line fitted

--- sigmoid function graph.py
+++ sigmoid function graph.py
@@ -17,5 +17,15 @@ X.shape, y.shape
 # remove Nan values from dataset, and sift through whether using age, gender or other marked values for our sigmoid function. 
 
 
-plt.scatter(x=X, y=y, color='black')
+print(X.columns)
+#target(y) should be the y axis and dictate the 0 or 1 classification. For feature 'Race' I can use get_dummies to make change categorical into numerical output. 
+
+
+
+
+
+plt.scatter(x=X['Age_at_diagnosis'], y=y, color='black', s=100)
+plt.ylabel('Probability glioma diagnosis')
+plt.xlabel('Age at Diagnosis')
+plt.title('Probability of Glioma Diagnosis by Age', weight='bold')
 plt.show()
