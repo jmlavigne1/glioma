bf830fbdb91f7f9a757a4954427603157be11cff
Joe LaVigne <jmlavigne510@gmail.com>
2024-12-10 22:25:48 -0500

    model prediction

--- sigmoid function graph.py
+++ sigmoid function graph.py
@@ -12,19 +12,39 @@ glioma_grading_clinical_and_mutation_features = fetch_ucirepo(id=759)
 X = glioma_grading_clinical_and_mutation_features.data.features 
 y = glioma_grading_clinical_and_mutation_features.data.targets 
 
-X.shape, y.shape
 
 # remove Nan values from dataset, and sift through whether using age, gender or other marked values for our sigmoid function. 
+X_race= pd.get_dummies(X['Race'])
+X_race = X_race.astype(int)
+print(X_race)
+
+X = X.drop('Race', axis=1, inplace=False)
+print(X)
 
 
-print(X.columns)
+X = X.join(X_race)
+print(X)
 #target(y) should be the y axis and dictate the 0 or 1 classification. For feature 'Race' I can use get_dummies to make change categorical into numerical output. 
 
 
 
 
+#################
+from sklearn.linear_model import LogisticRegression
+model = LogisticRegression()
+model.fit(X.values,y.values)
+
+# Plug sample data into fitted model
+sample_x = np.linspace(-16.65, 33.35, 300).reshape(-1,1)
+probability = model.predict_proba(sample_x[:,1])
+
+# Plot logistic curve
+plt.plot(sample_x, probability, color='red', linewidth=3)
+plt.show()
+##########
 
 plt.scatter(x=X['Age_at_diagnosis'], y=y, color='black', s=100)
+plt.plot(sample_x, probability, color='red', linewidth=3)
 plt.ylabel('Probability glioma diagnosis')
 plt.xlabel('Age at Diagnosis')
 plt.title('Probability of Glioma Diagnosis by Age', weight='bold')
